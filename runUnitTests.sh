f91d05acc7e0f9b0060130b43231987c6bfe0b22
Steven <steve@ip-172-31-34-88.us-west-2.compute.internal>
2015-05-07 18:46:08 +0000

    NoIssue: activate metering units test

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -12,9 +12,9 @@ export DJANGO_SETTINGS_MODULE=paywall2.settings
 ./manage.py runserver 172.31.34.88:8000 &
 pid=$!
 
-sleep 20
+sleep 5
 
-#python -m metering.pyTests $1 $2
+python -m metering.pyTests $1 $2
 python -m subscription.pyTests $1 $2 
 python -m authorization.pyTests $1 $2
 
