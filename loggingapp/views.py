7fd1bd449b77bf38e191065e4db73f14069b3162
Ubuntu <ubuntu@ip-172-31-15-47.ec2.internal>
2015-05-08 07:32:37 +0000

    PW-3: IP based filtering v2

--- loggingapp/views.py
+++ loggingapp/views.py
@@ -46,52 +46,55 @@ class PageViewCreate(generics.CreateAPIView):
   queryset = PageViews2.objects.all()
   serializer_class = pageViewsSerializer
   def perform_create(self, serializer):
+    if ("pageViewDateTime" in self.request.data) and ("pageViewSession" in self.request.data):
+      su = Sessions2.objects.get(sessionId=self.request.data['pageViewSession'])
+      su.sessionEndDateTime = self.request.data['pageViewDateTime']
+      su.save()
     if ("partyId" in self.request.data) and ("pageViewSession" in self.request.data):
-      PartySessionAffiliation(partySessionAffiliationParty=Party.objects.get(partyId=self.request.data['partyId']),
-             partySessionAffiliationSession=Sessions2.objects.get(sessionId=self.request.data['pageViewSession'])).save()
-    
+      PartySessionAffiliation( 
+          partySessionAffiliationParty=Party.objects.get(partyId=self.request.data['partyId']),
+          partySessionAffiliationSession=Sessions2.objects.get(sessionId=self.request.data['pageViewSession'])
+      ).save()
+    #TODO: Check and create new IP row
+    if ("ipAddress" in self.request.data) and ("pageViewSession" in self.request.data):
+      IpSessionAffiliation(
+          ipSessionAffiliationIp=IpTableForLogging.objects.get(ipTableForLoggingIp=self.request.data['ipAddress']),
+          ipSessionAffiliationSession = Sessions2.objects.get(sessionId=self.request.data['pageViewSession'])
+      ).save()
     if serializer.is_valid():
       serializer.save()
      
-
-###############
-  #def perform_create(self, serializer):
-  #  pass
-    #self.request.pageViewURI
-    #PageViews2(pageViewDateTime=self.request.pageViewDateTime, 
-    #           pageViewURI=self.request.pageViewURI, 
-    #           pageViewSession=self.request.pageViewSession).save()
-    #if 'ip' in self.request.POST:
-    #  ipId = findOrCreateIp(self.request.POST['ip'])
-    #serializer.save()
-    #serializer.save(pageViewDateTime=self.request.POST['pageViewDateTime'], pageViewURI=self.request.POST['pageViewURI'], pageViewSession=self.request.POST['pageViewSession'])
-       
-
 #Page view queries based on start, end and partyId
 #TODO: Add filtering based on IP address
 class PageViewList(generics.ListAPIView):
-  queryset = PageViews2.objects.all()
   serializer_class = pageViewsSerializer
-#       def get_queryset(self):
-#         queryset = PageViews.objects.all()
-#         startDate = self.request.QUERY_PARAMS.get('startDatetime', None)
-#         endDate = self.request.QUERY_PARAMS.get('endDatetime', None)
-#         partyId = self.request.QUERY_PARAMS.get('partyId', None)
-#         if startDate is not None:
-#           start = getdatetime(startDate)
-#           queryset = queryset.filter(pageViewDateTime__gte=start)
-#         if endDate is not None:
-#           end = getdatetime(endDate)
-#           queryset = queryset.filter(pageViewDateTime__lte=end)
-#         #TODO: Joins?
-#         if partyId is not None:
-#           queryset = queryset.filter(pageViewSession__sessionUserId=partyId).filter(pageViewSession__sessionUserType="PARTY")
-#         return queryset
-#     
-#     def getdatetime(datetimestring):
-#       print datetimestring
-#       datetimearr = datetimestring.split(" ")
-#       len(datetimearr)
-#       datearr = datetimearr[0].split("-")
-#       timearr = datetimearr[1].split(":")
-#       return datetime.datetime(int(datearr[0]), int(datearr[1]), int(datearr[2]), int(timearr[0]), int(timearr[1]), int(timearr[2]))
+  def get_queryset(self):
+    queryset = PageViews2.objects.all()
+    startDate = self.request.QUERY_PARAMS.get('startDatetime', None)
+    endDate = self.request.QUERY_PARAMS.get('endDatetime', None)
+    partyId = self.request.QUERY_PARAMS.get('partyId', None)
+    ipAddress = self.request.QUERY_PARAMS.get('ip', None)
+    if startDate is not None:
+      start = getdatetime(startDate)
+      queryset = queryset.filter(pageViewDateTime__gte=start)
+    if endDate is not None:
+      end = getdatetime(endDate)
+      queryset = queryset.filter(pageViewDateTime__lte=end)
+    if partyId is not None:
+      psobjs = PartySessionAffiliation.objects.filter(partySessionAffiliationParty=partyId)
+      sobjs = Sessions2.objects.filter(sessionId__in=psobjs.values('partySessionAffiliationSession'))
+      queryset = queryset.filter(pageViewSession__in=sobjs.values('sessionId'))
+    if ipAddress is not None:
+      ipobjs = IpTableForLogging.objects.filter(ipTableIp=ipAddress)
+      ipsobjs = IpSessionAffiliation.objects.filter(ipSessionAffiliationIp_id__in=ipobjs.values('ipTableId'))
+      sobjs = Sessions2.objects.filter(sessionId__in=ipsobjs.values('ipSessionAffiliationSession'))
+      queryset = queryset.filter(pageViewSession__in=sobjs.values('sessionId'))
+    return queryset
+     
+def getdatetime(datetimestring):
+  print datetimestring
+  datetimearr = datetimestring.split(" ")
+  len(datetimearr)
+  datearr = datetimearr[0].split("-")
+  timearr = datetimearr[1].split(":")
+  return datetime.datetime(int(datearr[0]), int(datearr[1]), int(datearr[2]), int(timearr[0]), int(timearr[1]), int(timearr[2]))
