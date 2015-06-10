5f028485181d2d3bfc9298c4ec360aeb0549c1e9
steve <steve@getexp.com>
2015-06-10 01:49:12 +0000

    PW-6: temporary loging, warn, and subscription templates for Proxy server redirect

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import url
 from subscription import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Basic CRUD operations
@@ -13,5 +14,10 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9.]+)/renewal/$', views.SubscriptionRenewal.as_view()),
     url(r'^active/$', views.SubscriptionsActive.as_view()),
     url(r'^payments/$', views.SubscriptionsPayment.as_view()),
+
+    # Templates
+    url(r'^templates/block/$', TemplateView.as_view(template_name="subscription/block.html")),
+    url(r'^templates/login/$', TemplateView.as_view(template_name="subscription/login.html")),
+    url(r'^templates/warn/$', TemplateView.as_view(template_name="subscription/warn.html")),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
