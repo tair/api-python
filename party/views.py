2cf6cd27e27f6e177c10bbf35dcb4cbbe23c576f
steve <steve@getexp.com>
2015-07-02 19:11:04 +0000

    PW-56: Convert Paywall 1 subscription data to Paywall 2 database format

--- party/views.py
+++ party/views.py
@@ -5,6 +5,10 @@ from party.serializers import PartySerializer, IpRangeSerializer
 
 from common.views import GenericCRUDView
 
+import json
+from django.http import HttpResponse
+from rest_framework.views import APIView
+
 # top level: /parties/
 
 # Basic CRUD operation for Party and IpRange
@@ -21,3 +25,11 @@ class IpRangeCRUD(GenericCRUDView):
 
 
 #------------------- End of Basic CRUD operations --------------
+
+class OrganizationCRUD(APIView):
+    def get(self, request, format=None):
+        obj = Party.objects.all().filter(partyType='organization')
+        out = []
+        for entry in obj:
+            out.append(entry.name)
+        return HttpResponse(json.dumps(out), content_type="application/json")
