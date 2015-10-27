5b1e82e33a6bde1f08416c9031920b344a52da66
Qian Li <qianli1987@arabidopsis.org>
2015-10-27 14:06:18 -0700

    TAIR-2468 temporarily set obj to userIdentifier=1501492704

--- authentication/views.py
+++ authentication/views.py
@@ -45,7 +45,7 @@ class listcreateuser(GenericCRUDView):
       serializer = serializer_class(data=data)
       if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response("successfull from post function")
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
   def put(self, request, format=None):
