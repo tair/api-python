1997470547385cf2e81886992aff3dfbab53abb6
andreywork <andrey@arabidopsis.org>
2015-10-27 17:12:14 -0700

    pw-123 temppwd

--- authentication/views.py
+++ authentication/views.py
@@ -108,7 +108,10 @@ def ForgotPassword(request):
   if request.method == 'POST':
     if not 'user' in request.POST:
         return HttpResponse(json.dumps({"message": "No username provided"}), status=400)
+    if not 'temppwd' in request.POST:
+        return HttpResponse(json.dumps({"message": "No temppwd provided"}), status=400)
     requestUsername = request.POST.get('user')
+    temppwd = request.POST.get('temppwd')
     requestPartner = request.GET.get('partnerId')
     user = Credential.objects.filter(partnerId=requestPartner).filter(username=requestUsername)
     # https://demoapi.arabidopsis.org/credentials/register
@@ -117,12 +120,12 @@ def ForgotPassword(request):
         subject = "%s Reset Password For %s" % (user.username, user.email)
         message = "%s (%s)\n" \
                   "\n" \
-                  "Your temp password is 777 \n" \
-                  % (user.username, user.email)
+                  "Your temp password is %s \n" \
+                  % (user.username, user.email, temppwd)
         from_email = "info@phoenixbioinformatics.org"
         recipient_list = [user.email, "info@phoenixbioinformatics.org"]
         send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
-        return HttpResponse(json.dumps({'reset pwd email message':'success', 'username':user.username, 'useremail':user.email}), content_type="application/json")
+        return HttpResponse(json.dumps({'reset pwd email message':'success', 'username':user.username, 'useremail':user.email, 'temppwd':temppwd}), content_type="application/json")
     else:
       return HttpResponse(json.dumps({"message":"No such user"}), status=401)
 
