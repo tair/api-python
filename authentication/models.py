13180ce964e7450145c2700e7d1ccbe71f4bdc8c
Qian Li <qianli1987@arabidopsis.org>
2015-10-27 18:26:48 -0700

    TAIR-2468 update username

--- authentication/models.py
+++ authentication/models.py
@@ -6,7 +6,8 @@ import base64, hmac, hashlib
 # Create your models here.
 
 class Credential(models.Model):
-  username = models.CharField(max_length=32, db_index=True)
+  # username = models.CharField(max_length=32, db_index=True)
+  username = models.CharField(max_length=32)
   password = models.CharField(max_length=64)
   email = models.CharField(max_length=128, null=True)
   institution = models.CharField(max_length=64, null=True)
