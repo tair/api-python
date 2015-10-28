afb213a2612baf669bd47488a4731c0aa051d756
andreywork <andrey@arabidopsis.org>
2015-10-28 16:22:15 -0700

    PW-161

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [ 
     url(r'^$', views.listcreateuser.as_view(), name='listcreateuser'),
-    # PW-161 url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', views.login, name='login'),
     # PW-161 url(r'^register', views.registerUser, name='register'),
     # PW-123 /credintials/forgot
     url(r'^forgot', views.ForgotPassword, name='forgot'),
