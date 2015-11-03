8cb5a021a1ad4960da9e7605f0900c4ebafdedbe
andreywork <andrey@arabidopsis.org>
2015-11-02 17:39:52 -0800

    pw-123

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [ 
     url(r'^$', views.listcreateuser.as_view(), name='listcreateuser'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^resetPwd/$', views.resetPwd, name='resetPwd'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
