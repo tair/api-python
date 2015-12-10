ea6e84473024a04516b0596fceb5b5c7cfc8d664
Qian Li <qianli1987@arabidopsis.org>
2015-12-10 11:58:45 -0800

    PW-217 authentication validate

--- authentication/models.py
+++ authentication/models.py
@@ -25,6 +25,7 @@ class Credential(models.Model):
         digested = base64.b64encode(hmac.new(str(partyId).encode('ascii'), usu.password.encode('ascii'), hashlib.sha1).digest())
         if digested == secretKey:
           return True
+      #TODO: validation still fail
       pu = pu.first().consortiums.all()
       if Credential.objects.filter(partyId_id__in=pu.values('partyId')).exists():
         for usu in Credential.objects.filter(partyId_id__in=pu.values('partyId')):
