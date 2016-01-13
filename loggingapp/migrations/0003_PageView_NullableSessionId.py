1f22d65c826601d1d3959f92aa5c0fbbfd881e6b
YarikM <yarik@arabidopsis.org>
2016-01-12 18:17:37 -0800

    PW-160: EXPERIMENT 2016-01-12-18-17

---
+++ loggingapp/migrations/0003_PageView_NullableSessionId.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('loggingapp', '0002_pageview_ip'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='pageview',
+            name='sessionId',
+            field=models.CharField(max_length=250, null=True),
+        ),
+    ]
