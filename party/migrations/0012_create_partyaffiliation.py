6cafec0d333bc3e7fa32f93e15435d46d5867858
Qian Li <qianli1987@arabidopsis.org>
2016-02-24 18:42:01 -0800

    #PW-221 added migration files to dev

---
+++ party/migrations/0012_create_partyaffiliation.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('party', '0011_remove_affiliation'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PartyAffiliation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('childPartyId', models.ForeignKey(related_name='childPartyId', db_column=b'childPartyId', to='party.Party')),
+                ('parentPartyId', models.ForeignKey(related_name='parentPartyId', db_column=b'parentPartyId', to='party.Party')),
+            ],
+            options={
+                'db_table': 'PartyAffiliation',
+            },
+        ),
+        migrations.AddField(
+            model_name='party',
+            name='consortiums',
+            field=models.ManyToManyField(related_name='PartyAffiliation', through='party.PartyAffiliation', to='party.Party'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='partyaffiliation',
+            unique_together=set([('childPartyId', 'parentPartyId')]),
+        ),
+    ]
