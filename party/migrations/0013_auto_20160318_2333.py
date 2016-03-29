ff623c20f1fde5310abdecd247a51d46bc8ba757
EC2 Default User <ec2-user@ip-172-31-3-220.us-west-2.compute.internal>
2016-03-29 21:45:39 +0000

    PW-263: migrate id to affiliationid, add unique constraint to affiliation

---
+++ party/migrations/0013_auto_20160318_2333.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('party', '0012_create_partyaffiliation'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='partyaffiliation',
+            old_name='id',
+            new_name='partyAffiliationId',
+        ),
+    ]
