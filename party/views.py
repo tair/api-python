e3047eb4d3ec6433830f33aeecd827c2dd6f96a7
Qian Li <qianli1987@arabidopsis.org>
2016-06-28 16:06:55 -0700

    PW-316 create consortium credential using profile page filter fix

--- party/views.py
+++ party/views.py
@@ -213,7 +213,7 @@ class ConsortiumCRUD(GenericCRUDView):
         partySerializer = PartySerializer(party, data=data)
 
         #get credential
-        if Credential.objects.get(partyId = consortiumId):
+        if Credential.objects.filter(partyId = consortiumId).exists():
             credential = Credential.objects.get(partyId = consortiumId)
         else:
             credential = Credential(partyId = consortiumId)
