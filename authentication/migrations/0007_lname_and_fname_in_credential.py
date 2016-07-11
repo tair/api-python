c7045b08292be1b6040d296295517f6b04864cfa
techteam <dev@phoenixbioinformatics.org>
2016-07-11 07:12:43 +0000

    migration file for PW-275

---
+++ authentication/migrations/0007_lname_and_fname_in_credential.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0006_Credential_tbl_drop_name_column_PW-161'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='credential',
+            name='firstName',
+            field=models.CharField(max_length=32, null=True),
+        ),
+        migrations.AddField(
+            model_name='credential',
+            name='lastName',
+            field=models.CharField(max_length=32, null=True),
+        ),
+    ]
