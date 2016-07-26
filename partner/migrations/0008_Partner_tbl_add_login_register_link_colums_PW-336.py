b151d086fb5b7b841cd57c6786a202c099b48527
andreywork <andrey@arabidopsis.org>
2016-07-25 18:58:59 -0700

    PW-336 new columns in partner table - loginlink and registration link

---
+++ partner/migrations/0008_Partner_tbl_add_login_register_link_colums_PW-336.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('partner', '0007_Partner_description_addColumn_PW271'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='partner',
+            name='loginLink',
+            field=models.CharField(max_length=200, null=True),
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='registerLink',
+            field=models.CharField(max_length=200, null=True),
+        ),
+    ]
