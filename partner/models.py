97326f9b8ac7921eb082ce6ef32e6336ccf409c3
andreywork <andrey@arabidopsis.org>
2016-08-04 11:29:31 -0700

    PW-342 forgot username email body

--- partner/models.py
+++ partner/models.py
@@ -18,7 +18,8 @@ class Partner(models.Model):
     subscriptionListDesc = models.CharField(max_length=1000, null=True)
     registerText = models.CharField(max_length=50, null=True)
     forgotUserNameEmailTo = models.CharField(max_length=128, null=True)
-    forgotUserNameEmailSubject = models.CharField(max_length=100, null=True) 
+    forgotUserNameEmailSubject = models.CharField(max_length=100, null=True)
+    forgotUserNameEmailBody = models.CharField(max_length=1000, null=True) 
     class Meta:
         db_table = "Partner"
 
