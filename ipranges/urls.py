e0f63229668a0743deb95a74bad124843cfccffb
andreywork <andrey@arabidopsis.org>
2016-08-08 16:51:29 -0700

    pw-344

--- ipranges/urls.py
+++ ipranges/urls.py
@@ -1,7 +1,7 @@
 #Copyright 2015 Phoenix Bioinformatics Corporation. All rights reserved.
 
 from django.conf.urls import url
-from cookies import views
+from ipranges import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [ 
