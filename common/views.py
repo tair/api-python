836ea88d6081b3386935ebb6c48d00fdd276d464
Qian Li <qianli1987@arabidopsis.org>
2016-08-24 18:25:35 -0700

    PW-267 apply token authentication to generic crud view

--- common/views.py
+++ common/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import generics, status
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 class GenericCRUDView(generics.GenericAPIView):
 
