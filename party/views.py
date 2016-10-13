a178599434e8e082f35166175c8db8bc452f18f4
andreywork <andrey@arabidopsis.org>
2016-10-13 11:44:04 -0700

    pw-277

--- party/views.py
+++ party/views.py
@@ -56,8 +56,11 @@ class PartyCRUD(GenericCRUDView):
     def get(self, request, format=None):
         ip = request.GET.get('ip')
         queryStr = 'select * from Party where partyId = (SELECT partyId FROM IpRange WHERE (INET_ATON("%s") BETWEEN INET_ATON(start) AND INET_ATON(end))) and (partyType="organization" or partyType="consortium")'
-        org = Party.objects.raw(queryStr,[ip])
-        return HttpResponse(org)
+        out = []
+        party = Party.objects.raw(queryStr,[ip])
+        partySerializer = PartySerializer(party)
+        out.append(partySerializer.data)
+        return HttpResponse(json.dumps(out), content_type="application/json")
 
 # /ipranges/
 class IpRangeCRUD(GenericCRUDView):
