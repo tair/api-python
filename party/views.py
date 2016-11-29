90ec23dea5cf05a1070d4197a633de6c2dedc7a4
andreywork <andrey@arabidopsis.org>
2016-11-28 17:55:57 -0800

    TAIR-2786

--- party/views.py
+++ party/views.py
@@ -81,7 +81,7 @@ class PartyOrgCRUD(GenericCRUDView):
                 #out.append("{'partyId':'%s','partyName':'%s','subscribed':'%s'}" % (str(entry.partyId), str(entry.name), str(entry.subscribed)))
                 out.append(str(entry.name)+";")
                 logging.error("/parties/org/?ip=%s, partyId=%s, name=%s, subscribed=%s" % (ip, entry.partyId, entry.name, entry.subscribed))
-            return HttpResponse(out, content_type="application/json")
+            return HttpResponse(out)
 
         except Exception as e:
             logging.error("Exception in /parties/org/?ip=%s, %s" % (ip, traceback.format_exc()))
