5354921154cbb443c7b80eb3e97365ec7a985e50
andreywork <andrey@arabidopsis.org>
2016-11-28 16:13:02 -0800

    TAIR-2786

--- party/views.py
+++ party/views.py
@@ -76,7 +76,10 @@ class PartyOrgCRUD(GenericCRUDView):
             results = self.namedtuplefetchall(cursor)
             out = []
             for entry in results:
-                out.append('{partyId:'+str(entry.partyId)+'partyName:'+entry.name+'subscribed:'+str(entry.subscribed)+'}')
+                #out.append('{"partyId":'+str(entry.partyId)+', "partyName":'+entry.name+', "subscribed":'+str(entry.subscribed)+'}')
+                out.append('partyId'+':'+str(entry.partyId))
+                out.append('partyName'+':'+str(entry.partyName))
+                
                 logging.error("/parties/org/?ip=%s, partyId=%s, name=%s, subscribed=%s" % (ip, entry.partyId, entry.name, entry.subscribed))
             return HttpResponse(json.dumps(out), content_type="application/json")
         except Exception as e:
