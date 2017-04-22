1201926fee1f175982929896896fbb5c3f54e589
Qian Li <qianli1987@arabidopsis.org>
2017-04-21 19:05:39 -0700

    swagger test fix

--- authentication/views.py
+++ authentication/views.py
@@ -30,10 +30,6 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Pastebin API')
 
-urlpatterns = [
-    url(r'^$', schema_view)
-]
-
 
 # Create your views here.
 
