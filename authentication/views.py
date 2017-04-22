73ed6cf0a5d19d68e82ef1bc8f8c452160489ff2
Qian Li <qianli1987@arabidopsis.org>
2017-04-21 17:52:46 -0700

    swagger test

--- authentication/views.py
+++ authentication/views.py
@@ -25,6 +25,16 @@ from common.permissions import isPhoenix
 
 import logging
 
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+urlpatterns = [
+    url(r'^$', schema_view)
+]
+
+
 # Create your views here.
 
 #/credentials/
