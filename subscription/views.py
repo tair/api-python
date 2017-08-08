54e99276d76607fa43a9efeea6259de582133ed4
Qian Li <qianli1987@arabidopsis.org>
2017-08-07 18:50:12 -0700

    PW-470 POST /subscriptions response type fix

--- subscription/views.py
+++ subscription/views.py
@@ -151,7 +151,7 @@ class SubscriptionCRUD(GenericCRUDView):
             serializer = self.serializer_class(data=request.data)
             # basic subscription creation
             if not isPhoenix(self.request):
-                return HttpResponse({'error': 'POST subscriptions/ credentialId and secretKey query parameters missing or invalid'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': 'POST subscriptions/ credentialId and secretKey query parameters missing or invalid'}, status=status.HTTP_400_BAD_REQUEST)
 
             if serializer.is_valid():
                 subscription = serializer.save()
