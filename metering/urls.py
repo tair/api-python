4922b56c8d433799b1c68e8c7659364b3c17d297
Qian Li <qianli1987@arabidopsis.org>
2017-10-27 15:37:47 -0700

    PWL-451 change regex to allow ipv6

--- metering/urls.py
+++ metering/urls.py
@@ -9,8 +9,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.IpAddressCountCRUD.as_view()),
-    url(r'^ip/(?P<ip>[0-9.]+)/increment/$', views.increment.as_view(), name='increment'),
-    url(r'^ip/(?P<ip>[0-9.]+)/limit/$', views.check_limit.as_view(), name='check_limit'),
+    url(r'^ip/(?P<ip>[0-9a-zA-Z.]+)/increment/$', views.increment.as_view(), name='increment'),
+    url(r'^ip/(?P<ip>[0-9a-zA-Z.]+)/limit/$', views.check_limit.as_view(), name='check_limit'),
     url(r'^limits/$', views.LimitValueCRUD.as_view()),
     url(r'^meterblacklist/$', views.MeterBlacklistCRUD.as_view()),
 ]
