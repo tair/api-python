94897f2326c8b1ac8ffc5b644ca626d5dab93b24
Qian Li <qianli1987@arabidopsis.org>
2018-01-23 17:02:38 -0800

    PHX-497 define meter choices

--- loggingapp/models.py
+++ loggingapp/models.py
@@ -16,7 +16,19 @@ class PageView(models.Model):
   ipList = models.CharField(max_length=250, null=True)
   partnerId = models.ForeignKey(Partner, db_column='partnerId', null=True)
   isPaidContent = models.NullBooleanField()
-  meterStatus = models.CharField(max_length=1, null=True)
+
+  # meter choices
+  METER_WARNING_STATUS = 'W'
+  METER_BLOCK_STATUS = 'B'
+  METER_MUST_SUBSCRIBE_STATUS = 'M'
+  METER_NOT_METERED_STATUS = 'N'
+  METER_STATUS_CHOICES = (
+    (METER_WARNING_STATUS, 'Warning'),
+    (METER_BLOCK_STATUS, 'Block'),
+    (METER_MUST_SUBSCRIBE_STATUS, 'Must subscribe'),
+    (METER_NOT_METERED_STATUS, 'Not metered'),
+  )
+  meterStatus = models.CharField(max_length=1, choices=METER_STATUS_CHOICES, null=True)
 
   class Meta:
     db_table = "PageView"
