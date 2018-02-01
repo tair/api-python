1fec02b54ca7457906fbd7b1c61432bb520c20ca
Qian Li <qianli1987@arabidopsis.org>
2018-01-31 18:28:22 -0800

    PWL-579 remove consortium subscription function

--- scripts/updateConsortiumSubscriptionFields.py
+++ scripts/updateConsortiumSubscriptionFields.py
@@ -17,27 +17,55 @@ partners = Partner.objects.all()
 for institution in institutions:
     for partner in partners:
         consortiumIdList = institution.consortiums.all().values_list('partyId', flat=True)
-        subscription = Subscription.objects.all().filter(partyId__in=consortiumIdList)\
+        updateData = {}
+        # find active consortium subscriptions with largest end date
+        activeSubscription = Subscription.objects.all().filter(partyId__in=consortiumIdList)\
             .filter(partnerId=partner)\
             .filter(startDate__lte=timezone.now())\
             .filter(endDate__gte=timezone.now()) \
             .order_by('-endDate').first()
-        # if any consortium has an active subscription for the certain partner
-        if subscription:
-            updateData = {}
-            updateData['consortiumStartDate'] = subscription.startDate
-            updateData['consortiumEndDate'] = subscription.endDate
-            updateData['consortiumId'] = subscription.partyId
+        # if exists set to update data
+        if activeSubscription:
+            consortiumSubscribed = True
+            updateData['consortiumStartDate'] = activeSubscription.startDate
+            updateData['consortiumEndDate'] = activeSubscription.endDate
+            updateData['consortiumId'] = activeSubscription.partyId
+        # if not exist, find all consortium subscriptions with largest end date
+        else:
+            subscription = Subscription.objects.all().filter(partyId__in=consortiumIdList)\
+                .filter(partnerId=partner)\
+                .order_by('-endDate').first()
+            # if exists set to update data
+            if subscription:
+                consortiumSubscribed = True
+                updateData['consortiumStartDate'] = subscription.startDate
+                updateData['consortiumEndDate'] = subscription.endDate
+                updateData['consortiumId'] = subscription.partyId
+            else:
+                consortiumSubscribed = False
+                updateData['consortiumStartDate'] = None
+                updateData['consortiumEndDate'] = None
+                updateData['consortiumId'] = None
+
+        created = False
+        obj = None
+        if consortiumSubscribed:
             # if the obj doesn't exist then create
             obj, created = Subscription.objects.get_or_create(
                 partnerId=partner,
                 partyId=institution,
                 defaults=updateData,
             )
-            # if the obj exists then update
-            if not created:
-                for k, v in updateData.iteritems():
-                    if getattr(obj, k) != v:
-                        setattr(obj, k, v)
+        else:
+            try:
+                obj = Subscription.objects.get(partyId=institution, partnerId=partner)
+            except Subscription.DoesNotExist:
+                continue
+        # if the obj exists then update
+        if not created:
+            for k, v in updateData.iteritems():
+                if getattr(obj, k) != v:
+                    setattr(obj, k, v)
+                    obj.save()
 
 
