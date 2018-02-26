5a683f505d4bf19a6f40d5fb5f398faf9fb7dfda
Qian Li <qianli1987@arabidopsis.org>
2018-02-26 15:09:53 -0800

    PWL-587 SerializerMethodField to return number of ip ranges

--- party/serializers.py
+++ party/serializers.py
@@ -5,9 +5,12 @@ from party.models import Party, IpRange, Country
 from rest_framework import serializers
 
 class PartySerializer(serializers.ModelSerializer):
+    ipRangeCount = serializers.SerializerMethodField() # Used SerializerMethodField to add a customized field which returns number of ip ranges
     class Meta:
         model = Party
-        fields = ('partyId','partyType', 'name', 'country', 'display', 'consortiums', 'label')
+        fields = ('partyId','partyType', 'name', 'country', 'display', 'consortiums', 'label', 'ipRangeCount')
+    def get_ipRangeCount(self, obj): # Method for SerializerMethodField
+        return obj.iprange_set.count()
 
 class IpRangeSerializer(serializers.ModelSerializer):
     class Meta:
