895a17765e0dd1cc23905ebb78f8685e918636ef
ChenXingguo <xingguochen@alumni.stanford.edu>
2018-10-24 15:10:46 -0700

    PWL-645: Assume param uri for check_limit method is encoded. Decode when receive request.

--- metering/views.py
+++ metering/views.py
@@ -76,7 +76,7 @@ class check_limit(APIView):
     '''
     def get(self, request, ip, format=None):
         partnerId = request.GET.get('partnerId')
-        uri = request.GET.get('uri')
+        uri = request.GET.get('uri').decode('utf8')
         """PW-287
          Change the check_limit() function to get the patterns for the specified partner by partnerId 
          and iterate through them to find any matches, 
