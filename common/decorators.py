fd482ce496a506157c956e1b5de54444f45e3458
Qian Li <qianli1987@arabidopsis.org>
2019-03-21 13:12:35 -0700

    PWL-738 decorator

---
+++ common/decorators.py
@@ -0,0 +1,18 @@
+from permissions import rolePermission
+from rest_framework.response import Response
+from rest_framework import status
+from functools import wraps
+from django.utils.decorators import available_attrs
+
+# This decorator is used to detect version of api and choose to apply authentication method
+def compatible_jwt(*roleList):
+    def decorator(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(request, *args, **kwargs):
+            if request.version == '2.0':
+                roleListStr = ','.join(roleList)
+                if not rolePermission(request, roleList):
+                    return Response({'error': 'roles needed: ' + roleListStr}, status=status.HTTP_400_BAD_REQUEST)
+            return func(request, *args, **kwargs)
+        return inner
+    return decorator
