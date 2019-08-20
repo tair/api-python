82b75ce11d891116fd8e6acfe299f2f456d486ec
ChenXingguo <xingguochen@alumni.stanford.edu>
2019-08-20 13:16:32 -0700

    PWL-773: Add unit test for API end point /nullservice. Since this module is not invoked for all servers, rename file to manualTests.py so it does not run when run batch tests.

---
+++ nullservice/manualTests.py
@@ -0,0 +1,30 @@
+import django
+import unittest
+import sys
+import json
+
+from django.test import TestCase
+from common.tests import TestGenericInterfaces, GenericTest
+from Cookie import SimpleCookie
+
+# Create your tests here.
+django.setup()
+serverUrl = TestGenericInterfaces.getHost()
+
+# test for API end point /nullservice
+class NullServiceTest(GenericTest, TestCase):
+    def test_for_get(self):
+        url = '%snullservice/' % (serverUrl)
+
+        self.client.cookies = SimpleCookie({'apiKey':self.apiKey})
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+print "Running unit tests on nullservice web services API........."
+
+if __name__ == '__main__':
+    sys.argv[1:] = []
+    unittest.main()
+    ret = not runner.run(suite).wasSuccessful()
+    sys.exit(ret)
