b8edfcd2c8b276b78e94cc9522e85b533f5fb9cc
root <root@ip-172-31-16-32.us-west-2.compute.internal>
2021-07-29 09:45:36 +0000

    CIPRES-71: Update email sending.

--- scripts/testEmailSending.py
+++ scripts/testEmailSending.py
@@ -5,12 +5,13 @@ os.sys.path.append('../')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paywall2.settings')
 django.setup()
 from subscription.controls import PaymentControl
-from partner.models import SubscriptionTerm
+from partner.models import SubscriptionTerm, Partner
 
 msg = "To access CIPRES resources, please visit phylo.org and log in using your CIPRES user account."
 purchaseId = 145
+partnerObj = Partner.objects.get(partnerId='cipres')
 termObj = SubscriptionTerm.objects.get(subscriptionTermId=20)
-emailAddress = 'xingguo.chen@arabidopsis.org'
+emailAddress = 'onmikula@gmail.com'
 firstname = 'Ondrej'
 lastname = 'Mikula'
 priceToCharge = 100
@@ -20,4 +21,4 @@ vat = ''
 try:
     PaymentControl.sendCIPRESEmail(msg, purchaseId, termObj, partnerObj, emailAddress, firstname, lastname, priceToCharge, institute, transactionId, vat)    
 except Exception, e:
-    message = print "Unexpected exception: %s" % (e)
+    print "Unexpected exception: %s" % (e)
