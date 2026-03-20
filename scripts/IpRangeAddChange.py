27ee665e8568f1f6ae4692522f37474f63e2a907
ChenXingguo <xingguochen@alumni.stanford.edu>
2026-03-19 23:57:12 -0700

    Fix minor bugs.

--- scripts/IpRangeAddChange.py
+++ scripts/IpRangeAddChange.py
@@ -58,10 +58,8 @@ for entry in IpRangeListData:
         # whitelist = set('abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.:')
         # startIp = ''.join(filter(whitelist.__contains__, startIp))
         # endIp = ''.join(filter(whitelist.__contains__, endIp))
-        startIp.replace(" ", "")
-        endIp.replace(" ", "")
-        startIp.strip()
-        endIp.strip()
+        startIp = startIp.replace(" ", "").strip()
+        endIp = endIp.replace(" ", "").strip()
 
         # create ip range from '-' format e.g. 1.2.3.0-255 ==> start: 1.2.3.0 end: 1.2.3.255
         if '-' in startIp:
@@ -118,7 +116,7 @@ for entry in IpRangeListData:
         except Exception as e:
             errlog.write('Error institution: ' + institutionName + ' ' + startIp + " " + endIp)
             errlog.write(str(e) + '\n')
-            raise Exception(str(e))
+            continue
 
     except (ValidationError, AddrFormatError, IntegrityError, UnicodeDecodeError) as e:
         errlog.write('Error institution: ' + institutionName + ' ' + startIp + " " + endIp + '\n')
@@ -144,10 +142,8 @@ for entry in IpRangeListData:
         # whitelist = set('abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.:')
         # startIp = ''.join(filter(whitelist.__contains__, startIp))
         # endIp = ''.join(filter(whitelist.__contains__, endIp))
-        startIp.replace(" ", "")
-        endIp.replace(" ", "")
-        startIp.strip()
-        endIp.strip()
+        startIp = startIp.replace(" ", "").strip()
+        endIp = endIp.replace(" ", "").strip()
 
         # create ip range from '-' format e.g. 1.2.3.0-255 ==> start: 1.2.3.0 end: 1.2.3.255
         if '-' in startIp:
